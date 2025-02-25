856bf8f2215218821f6d7a12e5c41196e2e41fc8
Dmytro <d.salyakin@gmail.com>
2025-02-25 22:17:58 +0200

    Add basic markup for components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
+import Description from '../Description/Description';
+import Feedback from '../Feedback/Feedback';
+import Options from '../Options/Options';
 import css from './App.module.css';
 
 export default function App() {
   return (
     <>
       <div className={css.container}>
-
+        <Description />
+        <Options />
+        <Feedback />
       </div>
     </>
   );
